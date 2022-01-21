94f08987c635de1aaa315262142811fcafe5ee0e
Big_Plate <great9744@gmail.com>
2022-01-21 18:37:58 +0900

    I cannot give it up now.

--- src/main/java/com/joo31775.java
+++ src/main/java/com/joo31775.java
@@ -1,11 +1,17 @@
 package com;
 
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.md_5.bungee.api.ChatMessageType;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
+
 
 public final class joo31775 extends JavaPlugin {
 
@@ -18,9 +24,9 @@ public final class joo31775 extends JavaPlugin {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-        Player player = e.getPlayer(); // key
+        Player player = e.getPlayer(); // The key
         player.sendMessage(ChatColor.BLUE + " Welcome."); // here doesn't work
-        player.sendMessage("Chatcolor error..?"); // here, either
+        player.sendMessage("Normal Welcome."); // here, either
     }
 
 
