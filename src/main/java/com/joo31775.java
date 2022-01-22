a1d63feb0c16b04c0124b57b67decdabe34bbe17
Big_Plate <great9744@gmail.com>
2022-01-22 16:51:34 +0900

    I don't know why this codes work well.

--- src/main/java/com/joo31775.java
+++ src/main/java/com/joo31775.java
@@ -5,8 +5,11 @@ package com;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -14,26 +17,26 @@ import org.jetbrains.annotations.NotNull;
 import java.util.EventListener;
 
 
-public final class joo31775 extends JavaPlugin implements EventListener {
+public final class joo31775 extends JavaPlugin implements EventListener, Listener {
 
     @Override
     public void onEnable() {
         getLogger().warning("server started."); //checked.
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
 
         // Plugin startup logic
     }
 
 
-    //  This following codes are not checked.
     @EventHandler
-    public void onPlayerJoin (PlayerJoinEvent event) {
+    public void onPlayerJoin(PlayerJoinEvent event) {
         getLogger().warning("Player JoinEvent triggered.");
-
-        @NotNull TextComponent text = Component.text("Welcome.") // Adventure API. remember!
-        .color(NamedTextColor.BLUE);
+        @NotNull TextComponent text = Component.text("Welcome") // Adventure API. remember!
+                .color(NamedTextColor.BLUE);
 
         Player player = event.getPlayer(); // The key
         player.sendActionBar(text);
+
     }
 
 
@@ -41,5 +44,6 @@ public final class joo31775 extends JavaPlugin implements EventListener {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
 
 }
