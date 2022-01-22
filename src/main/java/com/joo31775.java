14869fcaa796e15ad4e5d48ecbcd121fe7782a3a
Big_Plate <great9744@gmail.com>
2022-01-22 20:34:32 +0900

    Final set.

--- src/main/java/com/joo31775.java
+++ src/main/java/com/joo31775.java
@@ -1,42 +1,31 @@
 package com;
 
 
-
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 
-public final class joo31775 extends JavaPlugin implements Listener {
+public final class joo31775 extends JavaPlugin implements Listener {  //here is listener
 
     @Override
     public void onEnable() {
         getLogger().warning("server started."); //checked.
-        Bukkit.getServer().getPluginManager().registerEvents(this, this);
-
-        // Plugin startup logic
+        Bukkit.getServer().getPluginManager().registerEvents(this, this); // using listener
     }
 
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         getLogger().warning("Player JoinEvent triggered.");
-        @NotNull TextComponent text = Component.text("Welcome") // Adventure API. remember!
-                .color(NamedTextColor.BLUE);
-
-        Player player = event.getPlayer(); // The key
-        player.sendActionBar(text);
+        event.getPlayer().sendActionBar(Component.text("Welcome",(NamedTextColor.BLUE)));
 
     }
-
 
     @Override
     public void onDisable() {
